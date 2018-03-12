9c861fb937dd0bfda0673b7690895f502bd9febe
Greg Svoboda <gregsvo@gmail.com>
2018-03-12 10:10:05 -0400

    Edit for default props

--- helloReact/public/app.jsx
+++ helloReact/public/app.jsx
@@ -1,4 +1,28 @@
+var Greeter = React.createClass({
+    getDefaultProps: function() {
+        return {
+            firstName: 'React',
+            message: 'This is a default props message'
+        };
+    },
+
+    render: function () {
+        var name = this.props.name;
+        var message = this.props.message;
+        return (
+            <div>
+                <h1>Hello {name}!!!</h1>
+                <p>
+                    {message}
+                </p>
+            </div>
+        );
+    }
+});
+
+var firstName = 'Greg';
+
 ReactDOM.render(
-    <h1>Hello React!!!</h1>,
+    <Greeter name={firstName} message='Message From The Props!' />,
     document.getElementById('app')
 );
